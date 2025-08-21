58e4b7c535b05f37cf4e294f90e3215e5d529403
yashaswiraj12 <yashaswiraj1212@gmail.com>
2025-08-22 03:53:35 +0530

    config done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 // never forget to add .js in the end of import path for files in nodejs
 // this is because nodejs does not support import without .js in the end of path
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -9,5 +10,6 @@ app.get('/', (req, res) => {
 })
 app.listen(ENV.PORT,()=>{
     console.log(`server is up and running on port ${ENV.PORT}`);
+    connectDB();
     
 })
