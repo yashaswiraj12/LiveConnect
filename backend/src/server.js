1c672081d358d5fe9c495a64a72fefadf77d537a
yashaswiraj12 <yashaswiraj1212@gmail.com>
2025-08-22 01:04:53 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+// never forget to add .js in the end of import path for files in nodejs
+// this is because nodejs does not support import without .js in the end of path
+import { ENV } from './config/env.js';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello slack");
+})
+app.listen(ENV.PORT,()=>{
+    console.log(`server is up and running on port ${ENV.PORT}`);
+    
+})
