520d0a6ee5dc61dece45ee797596a265bd105d77
yashaswiraj12 <yashaswiraj1212@gmail.com>
2025-08-22 06:13:50 +0530

    auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,32 @@ import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
 import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from './config/inngest.js';
+import { serve } from "inngest/express";
 
 
 const app = express();
+app.use(express.json());
+app.use(clerkMiddleware());
+// request.auth will be available in request object 
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get('/', (req, res) => {
     res.send("hello slack");
 })
-app.listen(ENV.PORT,()=>{
-    console.log(`server is up and running on port ${ENV.PORT}`);
-    connectDB();
-    
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+export default app;
