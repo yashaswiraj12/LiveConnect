759b6fd3e78179479aab4d0913d6bf2b1c35c937
yashaswiraj12 <yashaswiraj1212@gmail.com>
2025-08-24 13:13:41 +0530

    seeting sentry in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import {SignedOut,SignInButton,SignedIn,UserButton} from "@clerk/clerk-react"
-import { Navigate, Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import AuthPage from './pages/AuthPage';
+import React from "react";
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import * as Sentry from "@sentry/react";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
- return (
-  <>
-    <SignedIn>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<Navigate to={"/"} replace />} />
-      </Routes>
-    </SignedIn>
+  return (
+    <>
+      <SignedIn>
+        <SentryRoutes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </SentryRoutes>
+      </SignedIn>
 
-    <SignedOut>
-      <Routes>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="*" element={<Navigate to={"/auth"} replace />} />
-      </Routes>
-    </SignedOut>
-  </>
-);
+      <SignedOut>
+        <SentryRoutes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="*" element={<Navigate to={"/auth"} replace />} />
+        </SentryRoutes>
+      </SignedOut>
+    </>
+  );
 };
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import {BrowserRouter} from "react-router";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+import * as Sentry from "@sentry/react";
+import React from "react";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,12 +28,31 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        {" "}
-        <App />
+        <QueryClientProvider client={queryClient}>
+          {" "}
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
